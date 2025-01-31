a9cd59b3173e0543c672a8e94f50d6a11e182ad9
yeonee <leenayeon@sookmyung.ac.kr>
2025-02-01 02:56:27 +0900

    [Feat/#1] start layout

--- src/pages/intro/Start.jsx
+++ src/pages/intro/Start.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; 
 import styled from "styled-components";
 
 export default function Start(){
+    const navigate = useNavigate();
+    const [stage, setStage] = useState(0);
+
+    const GoSignIn =()=>{
+        navigate("/signin");
+    }
+
+    const GoLogIn =()=>{
+        navigate("/login");
+    }
+
+    const renderButton =()=>{
+        switch(stage){
+            case 0:
+                return(
+                    <span onClick={() => setStage(stage + 1)}>시작하기</span>
+                );
+            case 1:
+                return(
+                    <div>
+                        <span onClick={GoSignIn}>회원가입</span>
+                        <span onClick={GoLogIn}>로그인</span>
+                    </div>
+                )
+        }
+    }
     return(
-        <div></div>
+        <ButtonContainer>
+            {renderButton()}
+        </ButtonContainer>
     );
 }
+
+const ButtonContainer = styled.div`
+`;
