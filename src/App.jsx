52b2f93784b528e4da1f5136eacc317946e08841
yeonee <leenayeon@sookmyung.ac.kr>
2025-02-01 02:29:53 +0900

    [Feat/#1] route, component setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import './App.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import SignIn from "./pages/auth/SignIn";
+import LogIn from "./pages/auth/LogIn";
+import Start from "./pages/intro/Start";
+import OT from "./pages/intro/OT";
+import Entrance from "./pages/intro/Entrance";
 
 function App() {
 
   return (
     <>
-      <span>초기 세팅</span>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={Start} />
+            <Route path="/signin" element={SignIn} />
+            <Route path="/login" element={LogIn} />
+            <Route path="/ot" element={OT} />
+            <Route path="/welcome" element={Entrance} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   )
 }
