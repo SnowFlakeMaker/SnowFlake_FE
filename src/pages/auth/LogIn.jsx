406ba026a605b0d361706665814d0e9c9cc531ac
yeonee <leenayeon@sookmyung.ac.kr>
2025-02-01 17:37:55 +0900

    [Feat/#1] login layout

--- src/pages/auth/LogIn.jsx
+++ src/pages/auth/LogIn.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 export default function LogIn(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const postLogIn =()=>{
+        console.log(email, password);
+    }
     return(
-        <div></div>
+        <div>
+            <span>로그인</span><br/>
+            <span>이메일</span>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            <span>비밀번호</span>
+            <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <span onClick={postLogIn}>로그인하기</span>
+        </div>
     );
 }
