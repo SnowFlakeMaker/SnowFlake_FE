e7a4729c9b4c9ef3f26c879bad78b452bf13f0a6
yeonee <leenayeon@sookmyung.ac.kr>
2025-02-08 00:01:17 +0900

    [Feat/#1] theme파일 작성

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,8 +1,34 @@
 const theme = {
-    colors: { //임시
-        primary: "#007bff",
-        secondary: "#6c757d",
+    colors: { 
+        mainblue100 : "#E9F5FF",
+        mainblue200 : "#8AB3DD",
+        mainblue300 : "#7A8A98",
+        mainblue400 : "#384754",
+        black : "#000000",
+        gray : "#D9D9D9",
+        white : "#FFFFFF",
+        yellow : "#FFE100",
+    },
+    typograhpy : {
+      title24 : {
+        fontSize: "24px",
+        lineHeight: "26px",
+        fontWeight: "500", 
+        letterSpacing: "-0.02em",
       },
+      subtitle20 : {
+        fontSize: "20px",
+        lineHeight: "26px",
+        fontWeight: "500", 
+        letterSpacing: "-0.02em",
+      },
+      subtitle15 : {
+        fontSize: "15px",
+        lineHeight: "26px",
+        fontWeight: "500", 
+        letterSpacing: "-0.02em",
+      }
+    }
 };
 
 export default theme;
