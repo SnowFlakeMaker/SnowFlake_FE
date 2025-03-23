f50fbab20472b8680d078bd4d9168192949d7b79
yeonee <leenayeon@sookmyung.ac.kr>
2025-03-24 00:47:08 +0900

    Add: DateProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ import Entrance from "./pages/intro/Entrance";
 import Main from "./pages/main/Main";
 import InfoNew from "./pages/auth/InfoNew";
 import { TutorialProvider } from "./pages/intro/Tutorial";
+import { DateProvider } from "./components/main/DateContext";
+import StressEnding from "./pages/ending/StressEnding";
 
 function App() {
 
@@ -28,22 +30,25 @@ function App() {
   return (
     <CookiesProvider>
       <TutorialProvider>
-        <ThemeProvider theme={theme}>
-        <GlobalStyle/>
-        
-          <Router>
-            <Routes>
-              <Route path="/" element={<Start/>} />
-              <Route path="/signin" element={<SignIn/>} />
-              <Route path="/login" element={<LogIn/>} />
-              <Route path="/info" element={<InfoNew/>} />
-              <Route path="/entrance" element={<Entrance />} />
-              <Route path="/ot" element={<OT/>} />
-              <Route path="/welcome" element={<Entrance/>} />
-              <Route path="/main" element={<Main/>}/>
-            </Routes>
-          </Router>
-        </ThemeProvider>
+        <DateProvider>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle/>
+          
+            <Router>
+              <Routes>
+                <Route path="/" element={<Start/>} />
+                <Route path="/signin" element={<SignIn/>} />
+                <Route path="/login" element={<LogIn/>} />
+                <Route path="/info" element={<InfoNew/>} />
+                <Route path="/entrance" element={<Entrance />} />
+                <Route path="/ot" element={<OT/>} />
+                <Route path="/welcome" element={<Entrance/>} />
+                <Route path="/main" element={<Main/>}/>
+                <Route path="/stress_ending" element={<StressEnding/>} />
+              </Routes>
+            </Router>
+          </ThemeProvider>
+        </DateProvider>
       </TutorialProvider>
     </CookiesProvider>
   )
