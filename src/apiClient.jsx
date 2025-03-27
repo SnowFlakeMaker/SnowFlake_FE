e724aa3a028bb7f53e41a2f4e60834ccd38e1499
yeonee <leenayeon@sookmyung.ac.kr>
2025-03-27 18:28:16 +0900

    Fix: apiClient 주소 수정

--- src/apiClient.jsx
+++ src/apiClient.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 export const apiClient = axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.VITE_SERVER_URL + "/api",
     withCredentials: true  // ✅ 모든 요청에 쿠키 포함
 });
 
